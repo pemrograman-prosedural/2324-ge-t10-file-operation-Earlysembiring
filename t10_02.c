e306ce766a1742e16ce6a8619763f48c15dd3907
Earlysembiring <nathaliasembiring06@gmail.com>
2024-05-06 16:53:32 +0700

    PAIR 05

--- t10_02.c
+++ t10_02.c
@@ -1,13 +1,22 @@
-// NIM - Name
-// NIM - Name
+// 12S23018 - Early Sembiring
+// 12S23027 - Amos Manurung
+
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "./libs/dorm.h"
 #include "./libs/student.h"
 #include "./libs/repository.h"
+#define _SIZE 255
 
 int main(int _argc, char **_argv)
 {
-    // codes
-    return 0;
-}
+    Dorm *dorms = (Dorm*) malloc(1 * sizeof(Dorm));
+    Student *students = (Student*) malloc(1 * sizeof(Student));
+    unsigned short totalDorm = 0;
+    unsigned short totalStudent = 0;
+    char line[_SIZE];
+    char* token;
+    char* delim = "#"; 
+    char* comand; 
